7bbb6aca5390601a504709c72e5309e8846da25a
1 <wrasf175@naver.com>
2023-06-15 01:37:31 +0900

    dev: openai, dotenv 패키지 설치

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__,
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
+@app.route("/palette", method=["POST"])
+def prompt_to_palette():
+    # TODO : OPEN AI Completion call 호출부 작성
+    return ''
 
 if __name__ == "__main__":
     app.run(debug=True)
