4dbfdf5bcc4ba21a46c3dd2327674f2c09ea2a13
1 <wrasf175@naver.com>
2023-06-15 02:15:29 +0900

    feat: completion API 연동 테스트

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config['OPENAI_API_KEY']
 
 app = Flask(__name__,
-    template_folder='templates',
-)
+            template_folder='templates',
+            )
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = openai.Completion.create(
+        prompt="Give me a funny word: ",
+        model="text-davinci-003",
+    )
 
-@app.route("/palette", method=["POST"])
+    return response["choices"][0]["text"]
+
+
+@app.route("/palette", methods=["POST"])
 def prompt_to_palette():
     # TODO : OPEN AI Completion call 호출부 작성
     return ''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
